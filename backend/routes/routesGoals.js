56e676d6239f3a63f4313fdbb8593c5eb128f374
MICHAELKITH <michaelkithinji27@gmail.com>
2022-11-04 08:19:49 +0300

    Initial commit

--- backend/routes/routesGoals.js
+++ backend/routes/routesGoals.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) =>{
-    res.status(200).json({  data: 'hello mike'})
-})
+    res.status(200).json({  data: 'Get goals'})
+});
+
+router.post('/', (req, res) =>{
+    res.status(200).json({  data: 'post goals '})
+});
+
+router.put('/:id', (req, res) =>{
+    res.status(200).json({data : `update goals ${req.params.id}`})
+});
+
+router.delete('/:id', (req, res) =>{
+    res.status(200).json({data : `delete  goals ${req.params.id}`})
+});
 
 module.exports = router;
