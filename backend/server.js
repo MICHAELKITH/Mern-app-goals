830704a8afc447463fef6ce513d6bd77bdad6ede
MICHAELKITH <michaelkithinji27@gmail.com>
2022-11-10 17:43:30 +0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const express = require('express')
-
+const colors = require ("colors")
+const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require("./middleware/errorMiddleware")
+const port = process.env.PORT || 3000;
 
+connectDB()
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-const port = process.env.PORT || 3000;
+
 
 app.use('/api/goals' , require('./routes/routesGoals'));
 app.use(errorHandler)
-
 app.listen(port, ()=>console.log(`app listening on ${port}`))
